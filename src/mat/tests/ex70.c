7f809910e2bafe055242a87d70afd114664ffaf8
Stefano Zampini <stefano.zampini@gmail.com>
2020-05-12 00:46:13 +0300

    Mat ex70: test MatMat operations callbacks with MATSHELL

--- src/mat/tests/ex70.c
+++ src/mat/tests/ex70.c
@@ -24,15 +24,115 @@ static PetscErrorCode CheckLocal(Mat A, Mat B, PetscScalar *a, PetscScalar *b)
   PetscFunctionReturn(0);
 }
 
+PetscErrorCode proj_mult(Mat S, Vec X, Vec Y)
+{
+  Mat            A,R,P;
+  Vec            Ax,Ay;
+  Vec            Px,Py;
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = PetscObjectQuery((PetscObject)S,"_testme_A",(PetscObject*)&A);CHKERRQ(ierr);
+  ierr = PetscObjectQuery((PetscObject)S,"_testme_P",(PetscObject*)&P);CHKERRQ(ierr);
+  ierr = PetscObjectQuery((PetscObject)S,"_testme_R",(PetscObject*)&R);CHKERRQ(ierr);
+  if (!A) SETERRQ(PetscObjectComm((PetscObject)S),PETSC_ERR_PLIB,"Missing matrix");
+  if (!R && !P) SETERRQ(PetscObjectComm((PetscObject)S),PETSC_ERR_PLIB,"Missing projectors");
+  if (R && P) SETERRQ(PetscObjectComm((PetscObject)S),PETSC_ERR_PLIB,"Both projectors");
+  ierr = MatCreateVecs(A,&Ax,&Ay);CHKERRQ(ierr);
+  if (R) {
+    ierr = MatCreateVecs(R,&Py,&Px);CHKERRQ(ierr);
+  } else {
+    ierr = MatCreateVecs(P,&Px,&Py);CHKERRQ(ierr);
+  }
+  ierr = VecCopy(X,Px);CHKERRQ(ierr);
+  if (P) {
+    ierr = MatMult(P,Px,Py);CHKERRQ(ierr);
+  } else {
+    ierr = MatMultTranspose(R,Px,Py);CHKERRQ(ierr);
+  }
+  ierr = VecCopy(Py,Ax);CHKERRQ(ierr);
+  ierr = MatMult(A,Ax,Ay);CHKERRQ(ierr);
+  ierr = VecCopy(Ay,Py);CHKERRQ(ierr);
+  if (P) {
+    ierr = MatMultTranspose(P,Py,Px);CHKERRQ(ierr);
+  } else {
+    ierr = MatMult(R,Py,Px);CHKERRQ(ierr);
+  }
+  ierr = VecCopy(Px,Y);CHKERRQ(ierr);
+  ierr = VecDestroy(&Px);CHKERRQ(ierr);
+  ierr = VecDestroy(&Py);CHKERRQ(ierr);
+  ierr = VecDestroy(&Ax);CHKERRQ(ierr);
+  ierr = VecDestroy(&Ay);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+PetscErrorCode MyPtShellPMultNumeric(Mat S, Mat P, Mat PtAP)
+{
+  Mat            A;
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = MatShellGetContext(S,(void**)&A);CHKERRQ(ierr);
+  ierr = PetscObjectCompose((PetscObject)PtAP,"_testme_A",(PetscObject)A);CHKERRQ(ierr);
+  ierr = PetscObjectCompose((PetscObject)PtAP,"_testme_P",(PetscObject)P);CHKERRQ(ierr);
+  ierr = PetscObjectCompose((PetscObject)PtAP,"_testme_R",NULL);CHKERRQ(ierr);
+  ierr = MatShellSetOperation(PtAP,MATOP_MULT,(void (*)(void))proj_mult);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+PetscErrorCode MyRShellRtMultNumeric(Mat S, Mat R, Mat RARt)
+{
+  Mat            A;
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = MatShellGetContext(S,(void**)&A);CHKERRQ(ierr);
+  ierr = PetscObjectCompose((PetscObject)RARt,"_testme_A",(PetscObject)A);CHKERRQ(ierr);
+  ierr = PetscObjectCompose((PetscObject)RARt,"_testme_R",(PetscObject)R);CHKERRQ(ierr);
+  ierr = PetscObjectCompose((PetscObject)RARt,"_testme_P",NULL);CHKERRQ(ierr);
+  ierr = MatShellSetOperation(RARt,MATOP_MULT,(void (*)(void))proj_mult);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+/* cannot use directly C in this test, since MatMatMult numeric is this function itself
+   However, users will know how to access B and C (e.g. MatDenseGetArray) and perform the proper computation */
+PetscErrorCode MyMatShellMatMultNumeric(Mat S, Mat B, Mat C)
+{
+  PetscErrorCode ierr;
+  Mat            A,CT;
+
+  PetscFunctionBegin;
+  ierr = MatShellGetContext(S,(void**)&A);CHKERRQ(ierr);
+  ierr = MatMatMult(A,B,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&CT);CHKERRQ(ierr);
+  ierr = MatCopy(CT,C,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+  ierr = MatDestroy(&CT);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+/* cannot use directly C in this test, since MatTransposeMatMult numeric is this function itself
+   However, users will know how to access B and C (e.g. MatDenseGetArray) and perform the proper computation */
+PetscErrorCode MyMatTransposeShellMatMultNumeric(Mat S, Mat B, Mat C)
+{
+  PetscErrorCode ierr;
+  Mat            A,CT;
+
+  PetscFunctionBegin;
+  ierr = MatShellGetContext(S,(void**)&A);CHKERRQ(ierr);
+  ierr = MatTransposeMatMult(A,B,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&CT);CHKERRQ(ierr);
+  ierr = MatCopy(CT,C,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+  ierr = MatDestroy(&CT);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
 int main(int argc,char **args)
 {
-  Mat            X,B,A,T,T2;
-  PetscInt       m,n,k,M = 10,N = 10,K = 5, ldx = 3, ldb = 5;
+  Mat            X,B,A,T,T2,PtAP = NULL,RARt = NULL, R = NULL;
+  PetscInt       m,n,k,M = 10,N = 10,K = 5, ldx = 3, ldb = 5, ldr = 4;
   const char     *deft = MATAIJ;
   char           mattype[256];
   PetscBool      flg,symm = PETSC_FALSE,testtt = PETSC_TRUE, testnest = PETSC_TRUE, testtranspose = PETSC_TRUE, testcircular = PETSC_FALSE, local = PETSC_TRUE;
-  PetscBool      xgpu = PETSC_FALSE,bgpu = PETSC_FALSE;
-  PetscScalar    *dataX = NULL,*dataB = NULL;
+  PetscBool      xgpu = PETSC_FALSE, bgpu = PETSC_FALSE, testshellops = PETSC_FALSE, testproj = PETSC_TRUE, testrart = PETSC_TRUE;
+  PetscScalar    *dataX = NULL,*dataB = NULL, *dataR = NULL;
   PetscScalar    *aX,*aB;
   PetscReal      err;
   PetscErrorCode ierr;
@@ -49,8 +149,12 @@ int main(int argc,char **args)
   ierr = PetscOptionsGetBool(NULL,NULL,"-testnest",&testnest,NULL);CHKERRQ(ierr);
   ierr = PetscOptionsGetBool(NULL,NULL,"-testtt",&testtt,NULL);CHKERRQ(ierr);
   ierr = PetscOptionsGetBool(NULL,NULL,"-testcircular",&testcircular,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetBool(NULL,NULL,"-testshellops",&testshellops,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetBool(NULL,NULL,"-testproj",&testproj,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetBool(NULL,NULL,"-testrart",&testrart,NULL);CHKERRQ(ierr);
   ierr = PetscOptionsGetBool(NULL,NULL,"-xgpu",&xgpu,NULL);CHKERRQ(ierr);
   ierr = PetscOptionsGetBool(NULL,NULL,"-bgpu",&bgpu,NULL);CHKERRQ(ierr);
+  if (M != N) testproj = PETSC_FALSE;
 #if defined(PETSC_USE_COMPLEX)
   testtranspose = PETSC_FALSE;
   testtt = PETSC_FALSE;
@@ -103,6 +207,10 @@ int main(int argc,char **args)
   }
   ierr = MatCreateDense(PETSC_COMM_WORLD,n,PETSC_DECIDE,N,K,dataB,&B);CHKERRQ(ierr);
   ierr = MatCreateDense(PETSC_COMM_WORLD,m,PETSC_DECIDE,M,K,dataX,&X);CHKERRQ(ierr);
+  if (local) {
+    ierr = MatDenseSetLDA(X,m+ldx);CHKERRQ(ierr);
+    ierr = MatDenseSetLDA(B,n+ldb);CHKERRQ(ierr);
+  }
 
   /* store pointer to dense data for testing */
   ierr = MatDenseGetArrayRead(B,(const PetscScalar**)&dataB);CHKERRQ(ierr);
@@ -115,6 +223,7 @@ int main(int argc,char **args)
     dataX = aX;
     dataB = aB;
   }
+
   ierr = MatSetRandom(B,NULL);CHKERRQ(ierr);
   /* convert to CUDA if needed */
   if (bgpu) {
@@ -137,6 +246,38 @@ int main(int argc,char **args)
     ierr = MatAXPY(T,-1.0,X,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
     ierr = MatView(T,NULL);CHKERRQ(ierr);
     ierr = MatDestroy(&T);CHKERRQ(ierr);
+  }
+
+  /* Test projection operations (PtAP and RARt) */
+  if (testproj) {
+    ierr = MatPtAP(A,B,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&PtAP);CHKERRQ(ierr);
+    ierr = MatPtAPMultEqual(A,B,PtAP,10,&flg);CHKERRQ(ierr);
+    if (!flg) {
+      ierr = PetscPrintf(PETSC_COMM_WORLD,"Error with PtAP\n");CHKERRQ(ierr);
+      ierr = MatMatMult(A,B,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&T);CHKERRQ(ierr);
+      ierr = MatTransposeMatMult(B,T,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&T2);CHKERRQ(ierr);
+      ierr = MatAXPY(T2,-1.0,PtAP,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+      ierr = MatView(T2,NULL);CHKERRQ(ierr);
+      ierr = MatDestroy(&T2);CHKERRQ(ierr);
+      ierr = MatDestroy(&T);CHKERRQ(ierr);
+    }
+    ierr = MatGetLocalSize(B,NULL,&k);CHKERRQ(ierr);
+    ierr = PetscMalloc1(PetscMax((k+ldr)*M,1),&dataR);CHKERRQ(ierr);
+    ierr = MatCreateDense(PETSC_COMM_WORLD,PETSC_DECIDE,m,K,M,dataR,&R);CHKERRQ(ierr);
+    ierr = MatSetRandom(R,NULL);CHKERRQ(ierr);
+    if (testrart) { /* fails for AIJCUSPARSE */
+      ierr = MatRARt(A,R,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&RARt);CHKERRQ(ierr);
+      ierr = MatRARtMultEqual(A,R,RARt,10,&flg);CHKERRQ(ierr);
+      if (!flg) {
+        ierr = PetscPrintf(PETSC_COMM_WORLD,"Error with RARt\n");CHKERRQ(ierr);
+        ierr = MatMatTransposeMult(A,R,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&T);CHKERRQ(ierr);
+        ierr = MatMatMult(R,T,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&T2);CHKERRQ(ierr);
+        ierr = MatAXPY(T2,-1.0,RARt,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+        ierr = MatView(T2,NULL);CHKERRQ(ierr);
+        ierr = MatDestroy(&T2);CHKERRQ(ierr);
+        ierr = MatDestroy(&T);CHKERRQ(ierr);
+      }
+    }
   }
 
   /* Test MatDenseGetColumnVec and friends */
@@ -170,6 +311,19 @@ int main(int argc,char **args)
 
   /* Test with MatShell */
   ierr = MatConvert(A,MATSHELL,MAT_INITIAL_MATRIX,&T2);CHKERRQ(ierr);
+  if (testshellops) { /* test callbacks for user defined MatProducts */
+    ierr = MatShellSetMatProductOperation(T2,MATPRODUCT_AB,NULL,MyMatShellMatMultNumeric,MATDENSE,MATDENSE);CHKERRQ(ierr);
+    ierr = MatShellSetMatProductOperation(T2,MATPRODUCT_AB,NULL,MyMatShellMatMultNumeric,MATDENSECUDA,MATDENSECUDA);CHKERRQ(ierr);
+    ierr = MatShellSetMatProductOperation(T2,MATPRODUCT_AtB,NULL,MyMatTransposeShellMatMultNumeric,MATDENSE,MATDENSE);CHKERRQ(ierr);
+    ierr = MatShellSetMatProductOperation(T2,MATPRODUCT_AtB,NULL,MyMatTransposeShellMatMultNumeric,MATDENSECUDA,MATDENSECUDA);CHKERRQ(ierr);
+    if (testproj) {
+      ierr = MatShellSetMatProductOperation(T2,MATPRODUCT_PtAP,NULL,MyPtShellPMultNumeric,MATDENSE,MATSHELL);CHKERRQ(ierr);
+      ierr = MatShellSetMatProductOperation(T2,MATPRODUCT_PtAP,NULL,MyPtShellPMultNumeric,MATDENSECUDA,MATSHELL);CHKERRQ(ierr);
+      ierr = MatShellSetMatProductOperation(T2,MATPRODUCT_RARt,NULL,MyRShellRtMultNumeric,MATDENSE,MATSHELL);CHKERRQ(ierr);
+      ierr = MatShellSetMatProductOperation(T2,MATPRODUCT_RARt,NULL,MyRShellRtMultNumeric,MATDENSECUDA,MATSHELL);CHKERRQ(ierr);
+    }
+  }
+  ierr = CheckLocal(B,X,aB,aX);CHKERRQ(ierr);
   ierr = MatMatMult(T2,B,MAT_REUSE_MATRIX,PETSC_DEFAULT,&X);CHKERRQ(ierr);
   ierr = CheckLocal(B,X,aB,aX);CHKERRQ(ierr);
   ierr = MatMatMultEqual(T2,B,X,10,&flg);CHKERRQ(ierr);
@@ -179,6 +333,52 @@ int main(int argc,char **args)
     ierr = MatAXPY(T,-1.0,X,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
     ierr = MatView(T,NULL);CHKERRQ(ierr);
     ierr = MatDestroy(&T);CHKERRQ(ierr);
+  }
+  if (testproj) {
+    ierr = MatPtAPMultEqual(T2,B,PtAP,10,&flg);CHKERRQ(ierr);
+    if (!flg) {
+      ierr = PetscPrintf(PETSC_COMM_WORLD,"Error with PtAP\n");CHKERRQ(ierr);
+    }
+    if (testshellops) {
+      ierr = MatPtAP(T2,B,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&T);CHKERRQ(ierr);
+      ierr = MatPtAPMultEqual(T2,B,T,10,&flg);CHKERRQ(ierr);
+      if (!flg) {
+        Mat TE;
+
+        ierr = PetscPrintf(PETSC_COMM_WORLD,"Error with PtAP (user defined)\n");CHKERRQ(ierr);
+        ierr = MatComputeOperator(T,MATDENSE,&TE);CHKERRQ(ierr);
+        ierr = MatView(TE,NULL);CHKERRQ(ierr);
+        ierr = MatView(PtAP,NULL);CHKERRQ(ierr);
+        ierr = MatAXPY(TE,-1.0,PtAP,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+        ierr = MatView(TE,NULL);CHKERRQ(ierr);
+        ierr = MatDestroy(&TE);CHKERRQ(ierr);
+      }
+      ierr = MatDestroy(&T);CHKERRQ(ierr);
+    }
+    if (RARt) {
+      ierr = MatRARtMultEqual(T2,R,RARt,10,&flg);CHKERRQ(ierr);
+      if (!flg) {
+        ierr = PetscPrintf(PETSC_COMM_WORLD,"Error with RARt\n");CHKERRQ(ierr);
+      }
+    }
+    if (testshellops) {
+      ierr = MatRARt(T2,R,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&T);CHKERRQ(ierr);
+      ierr = MatRARtMultEqual(T2,R,T,10,&flg);CHKERRQ(ierr);
+      if (!flg) {
+        Mat TE;
+
+        ierr = PetscPrintf(PETSC_COMM_WORLD,"Error with RARt (user defined)\n");CHKERRQ(ierr);
+        ierr = MatComputeOperator(T,MATDENSE,&TE);CHKERRQ(ierr);
+        ierr = MatView(TE,NULL);CHKERRQ(ierr);
+        if (RARt) {
+          ierr = MatView(RARt,NULL);CHKERRQ(ierr);
+          ierr = MatAXPY(TE,-1.0,RARt,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+          ierr = MatView(TE,NULL);CHKERRQ(ierr);
+        }
+        ierr = MatDestroy(&TE);CHKERRQ(ierr);
+      }
+      ierr = MatDestroy(&T);CHKERRQ(ierr);
+    }
   }
 
   if (testtranspose) {
@@ -270,8 +470,12 @@ int main(int argc,char **args)
   ierr = MatDestroy(&X);CHKERRQ(ierr);
   ierr = MatDestroy(&B);CHKERRQ(ierr);
   ierr = MatDestroy(&A);CHKERRQ(ierr);
+  ierr = MatDestroy(&R);CHKERRQ(ierr);
+  ierr = MatDestroy(&PtAP);CHKERRQ(ierr);
+  ierr = MatDestroy(&RARt);CHKERRQ(ierr);
   ierr = PetscFree(dataX);CHKERRQ(ierr);
   ierr = PetscFree(dataB);CHKERRQ(ierr);
+  ierr = PetscFree(dataR);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return ierr;
 }
@@ -280,13 +484,13 @@ int main(int argc,char **args)
 
   test:
     suffix: 1
-    args: -local {{0 1}}
+    args: -local {{0 1}} -testshellops
 
   test:
     output_file: output/ex70_1.out
     requires: cuda
     suffix: 1_cuda
-    args: -local {{0 1}} -xgpu {{0 1}} -bgpu {{0 1}} -A_mat_type {{seqaijcusparse seqaij}} -testnest 0
+    args: -local {{0 1}} -xgpu {{0 1}} -bgpu {{0 1}} -A_mat_type {{seqaijcusparse seqaij}} -testnest 0 -testshellops {{0 1}} -testrart 0
 
   test:
     TODO: VecGetSubVector seems broken with CUDA
@@ -306,7 +510,7 @@ int main(int argc,char **args)
     requires: cuda
     nsize: 2
     suffix: 1_par_cuda
-    args: -testtranspose 0 -local {{0 1}} -xgpu {{0 1}} -bgpu {{0 1}} -A_mat_type {{mpiaijcusparse mpiaij}} -testnest 0
+    args: -testtranspose 0 -local {{0 1}} -xgpu {{0 1}} -bgpu {{0 1}} -A_mat_type {{mpiaijcusparse mpiaij}} -testnest 0 -testrart 0
 
   test:
     output_file: output/ex70_1.out
@@ -338,7 +542,7 @@ int main(int argc,char **args)
     output_file: output/ex70_1.out
     suffix: 3
     nsize: {{1 3}}
-    args: -M 13 -N 13 -K {{1 3}} -local {{0 1}} -A_mat_type sbaij -symm -testtranspose 0
+    args: -M 13 -N 13 -K {{1 3}} -local {{0 1}} -A_mat_type sbaij -symm -testtranspose 0 -testproj 0
 
   test:
     output_file: output/ex70_1.out
@@ -368,13 +572,6 @@ int main(int argc,char **args)
   test:
     output_file: output/ex70_1.out
     suffix: 7
-    nsize: 1
-    args: -M 13 -N 13 -K {{1 3}} -local {{0 1}} -A_mat_type dense -testnest 0 -testcircular
-
-  test:
-    TODO: NEST x DENSE with dense nested matrices seems broken in this case
-    output_file: output/ex70_1.out
-    suffix: 7_broken
     nsize: 1
     args: -M 13 -N 13 -K {{1 3}} -local {{0 1}} -A_mat_type dense -testnest -testcircular
 
