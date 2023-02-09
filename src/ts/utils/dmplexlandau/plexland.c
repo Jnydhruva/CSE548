149c442bd6bc02f2732adc312c1c283e34575e76
Mark Adams <ma2325@columbia.edu>
2023-02-09 09:37:28 -0500

    format

--- src/ts/utils/dmplexlandau/plexland.c
+++ src/ts/utils/dmplexlandau/plexland.c
@@ -1063,7 +1063,7 @@ static PetscErrorCode LandauSetInitialCondition(DM dm, Vec X, PetscInt grid, Pet
 }
 
 // adapt a level once. Forest in/out
-char                 *s_refine_names[] = {"RE", "Z1", "Origin", "Z2", "Uniform"};
+static const char    *s_refine_names[] = {"RE", "Z1", "Origin", "Z2", "Uniform"};
 static PetscErrorCode adaptToleranceFEM(PetscFE fem, Vec sol, PetscInt type, PetscInt grid, LandauCtx *ctx, DM *newForest)
 {
   DM              forest, plex, adaptedDM = NULL;
