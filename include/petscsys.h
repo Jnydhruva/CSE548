Atomic-Industries/petsc

PetscOmpCtrl: add PetscOmpCtrlSetNumThreads()

include/petscsys.h
@@ -2573,6 +2573,7 @@ PETSC_EXTERN PetscErrorCode PetscOmpCtrlDestroy(PetscOmpCtrl *);
 PETSC_EXTERN PetscErrorCode PetscOmpCtrlBarrier(PetscOmpCtrl);
 PETSC_EXTERN PetscErrorCode PetscOmpCtrlOmpRegionOnMasterBegin(PetscOmpCtrl);
 PETSC_EXTERN PetscErrorCode PetscOmpCtrlOmpRegionOnMasterEnd(PetscOmpCtrl);
+PETSC_EXTERN PetscErrorCode PetscOmpCtrlSetNumThreads(PetscOmpCtrl, PetscInt);
 
 PETSC_EXTERN PetscErrorCode PetscSegBufferCreate(size_t, size_t, PetscSegBuffer *);
 PETSC_EXTERN PetscErrorCode PetscSegBufferDestroy(PetscSegBuffer *);

Jnydhruva/CSE548

include/petscsys.h
@@ -2220,6 +2220,7 @@ PETSC_EXTERN PetscErrorCode PetscOmpCtrlDestroy(PetscOmpCtrl *);
 PETSC_EXTERN PetscErrorCode PetscOmpCtrlBarrier(PetscOmpCtrl);
 PETSC_EXTERN PetscErrorCode PetscOmpCtrlOmpRegionOnMasterBegin(PetscOmpCtrl);
 PETSC_EXTERN PetscErrorCode PetscOmpCtrlOmpRegionOnMasterEnd(PetscOmpCtrl);
+PETSC_EXTERN PetscErrorCode PetscOmpCtrlSetNumThreads(PetscOmpCtrl, PetscMPIInt);
 
 PETSC_EXTERN PetscErrorCode PetscSegBufferCreate(size_t, size_t, PetscSegBuffer *);
 PETSC_EXTERN PetscErrorCode PetscSegBufferDestroy(PetscSegBuffer *);